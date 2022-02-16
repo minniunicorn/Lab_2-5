9d77cc6a8b07a291eaad3a278feb49d4fec6d94a
minniunicorn <nknknkf92@gmail.com>
2022-02-16 15:17:13 +0300

    Добавьте файлы проекта.

---
+++ ArrayClass.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_5
+{
+    class ArrayClass
+    {
+        /// <summary>
+        /// Массив
+        /// </summary>
+        public int[] vs = new int[10];
+        /// <summary>
+        /// Максимальный элемент
+        /// </summary>
+        public int max;
+        /// <summary>
+        /// Минимальный элемент
+        /// </summary>
+        public int min;
+        /// <summary>
+        /// Индекс максимального элемента
+        /// </summary>
+        public int temp1;
+        public static void Output(int[] vs)
+        {
+            for (int i = 0; i < vs.Length; i++)
+            {
+                Console.Write(vs[i] + " ");
+            }
+        }
+        public static int Max(int[] vs, int max)
+        {
+            for (int i = 0; i < vs.Length; i++)
+            {
+                if (vs[i]>max)
+                {
+                    max = vs[i];
+                }
+            }
+            return max;
+        }       
+        public static int Min(int[] vs, int min)
+        {
+            for (int i = 0; i < vs.Length; i++)
+            {
+                if (vs[i] < min)
+                {
+                    min = vs[i];
+                }
+            }
+            return min;
+        }
+
+        public static void Change(int[] vs, int max, int min, int temp1)
+        {
+            max = 0;
+            min = 0;
+            for (int i = 1; i < vs.Length; i++)
+            {
+                if (vs[i]<vs[min])
+                {
+                    min = i;
+                }
+                else
+                {
+                    if (vs[i]>vs[max])
+                    {
+                        max = i;
+                    }
+                }
+            }
+            temp1 = vs[min];
+            vs[min] = vs[max];
+            vs[max] = temp1;           
+        }
+        public static void Sorting (int[] vs)
+        {
+            for (int j = vs.Length - 1; j > 0; j--)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    if (vs[i] > vs[i + 1])
+                    {
+                        int temp2 = vs[i];
+                        vs[i] = vs[i + 1];
+                        vs[i + 1] = temp2;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача на подпрограммы");
+
+            /****ВЫВОД МАССИВА****/
+            int[] array = new int[10];
+            Random random = new Random();
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(40);
+            }
+            ArrayClass.Output(array);
+            Console.WriteLine();
+
+            /****разность между максимальным и минимальным элементами массива****/
+            int max = 0;
+            int min = array[0];
+            int temp1 = 0;
+            max = ArrayClass.Max(array, max);
+            min = ArrayClass.Min(array, min);
+            Console.WriteLine("Max = " + max + ", Min = " + min);
+            Console.WriteLine();
+            int raz = max - min;
+            Console.WriteLine("Разность между максимальным и минимаьным = " + raz);
+
+            /****СОРТИРОВКА МАССИВА****/
+            Console.WriteLine();
+            ArrayClass.Change(array, max, min, temp1);
+            Console.WriteLine("Массив, в котором максимальный и минимальный элементы поменялись местами");
+            ArrayClass.Output(array);
+            Console.WriteLine();
+            Console.WriteLine("Отсортированный массив");
+            ArrayClass.Sorting(array);
+            ArrayClass.Output(array);
+            Console.ReadKey();
+        }
+    }
+}
